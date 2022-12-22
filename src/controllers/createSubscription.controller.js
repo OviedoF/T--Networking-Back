7f63027fcb76da80628356702dfc97917da9dd33
Emiliano Bartolacci <emibarto@live.com.ar>
2022-12-22 14:57:15 -0300

    actualizo back

--- src/controllers/createSubscription.controller.js
+++ src/controllers/createSubscription.controller.js
@@ -5,9 +5,26 @@ const CreateSubscription = {};
 
 CreateSubscription.getCreate = async (req, res) => {
     try {
+        const { filename } = req.files[0];
+        
+        const newSubscription = new Subscription({
+            ...req.body,
+            principalImage: `${process.env.ROOT_URL}/images/${filename}`
+        })
 
+        const savedSubscription = await newSubscription.save();
+
+        res.status(201).send({
+            savedSubscription,
+            message: 'Membresia creada correctamente!',
+            subscriptionData: {
+                name: req.body.name,
+            }
+        })
     } catch (error) {
     console.log(error);
     return res.status(500).send(error);
   }
 }
+
+module.exports = CreateSubscription
