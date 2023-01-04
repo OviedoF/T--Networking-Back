ad2900a682fbadbb584aaeedaed4112e6bfbcf3e
oviedoF <oviedofederico039@gmail.com>
2023-01-04 11:20:47 -0300

    Modelo membresía

--- src/models/membership.model.js
+++ src/models/membership.model.js
@@ -28,6 +28,54 @@ const membershipSchema = new Schema({
     image: {
         type: String,
         required: true
+    },
+
+    permissions: {
+        perfilPhoto: Boolean,
+        coverPhoto: Boolean,
+        enterprisePhoto: Boolean,
+        name: Boolean,
+        job: Boolean,
+        jobEntity: Boolean,
+        description: Number,
+        email: Boolean,
+        saveContact: Boolean,
+        vcard: Boolean,
+        social: {
+            favorites: {
+                available: Boolean,
+                exclude: Array,
+                obligatory: Array,
+                limit: Number
+            },
+            aditionals:{
+                available: Boolean,     
+                exclude: Array,
+                obligatory: Array,
+                limit: Number
+            },
+        },
+        qr: {
+            design: Boolean,
+            color: Boolean,
+            size: Boolean,
+            logo: Boolean,
+            logoSize: Boolean,
+            logoPosition: Boolean,
+        },
+        publicity: Boolean,
+        perfilType: Boolean,
+        userLink: Boolean,
+        design: {
+            header: {
+                perfilPhoto: Boolean,
+                logoPhoto: Boolean,
+            },
+            body: {
+                name: Boolean,
+                job: Boolean
+            }
+        }
     }
 }, {
     timestamps: true,
